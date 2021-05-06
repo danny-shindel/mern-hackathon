b97e82e9dc774a4a70c798a8e036d48fb6d39e51
Jim Clark <jim.clark@generalassemb.ly>
2021-05-06 09:50:33 -0700

    Add Order model

---
+++ models/order.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const itemSchema = require('./itemSchema');
+const Schema = mongoose.Schema;
+
+const lineItemSchema = new Schema({
+  qty: { type: Number, default: 1 },
+  item: itemSchema
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true }
+});
+
+lineItemSchema.virtual('extPrice').get(function() {
+  // 'this' is bound to the lineItem subdoc
+  return this.qty * this.item.price;
+});
+
+const orderSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User'},
+  lineItems: [lineItemSchema],
+  isPaid: { type: Boolean, default: false }
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true }
+});
+
+orderSchema.virtual('orderTotal').get(function() {
+  // 'this' is bound to the order doc
+  return this.lineItems.reduce((total, item) => total + item.extPrice, 0);
+});
+
+orderSchema.virtual('totalQty').get(function() {
+  // 'this' is bound to the order doc
+  return this.lineItems.reduce((total, item) => total + item.qty, 0);
+});
+
+orderSchema.virtual('orderId').get(function() {
+  // 'this' is bound to the order doc
+  return this.id.slice(-6).toUpperCase();
+});
+
+module.exports = mongoose.model('Order', orderSchema);

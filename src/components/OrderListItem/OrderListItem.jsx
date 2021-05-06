5fca48fe7960913c8714e6cd4763f44ece568426
Erwin Medina <erwin.l.medina@gmail.com>
2021-05-06 15:32:27 -0700

    finalized css

--- src/components/OrderListItem/OrderListItem.jsx
+++ src/components/OrderListItem/OrderListItem.jsx
@@ -4,9 +4,14 @@ export default function OrderListItem({ order , setOrder, selected }) {
   
   return (
     <div className="OrderListItem" id={selected === order && "selected"} onClick={()=>setOrder(order)}>
-      <div>{order.totalQty} Items</div>
-      <div>Order ID:{order.orderId}</div>
-      <div>${order.orderTotal.toFixed(2)}</div>
+      <div>
+        <div>Order ID: {order.orderId}</div>
+        <div>{new Date(order.updatedAt).toLocaleDateString()}</div>
+      </div>
+      <div id="right">
+        <div>${order.orderTotal.toFixed(2)}</div>
+        <div>{order.totalQty} Items</div>
+      </div>
     </div>
   );
 }
